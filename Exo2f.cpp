1f6a4833b04cb03058b14200f8f21091397b7a47
reda-coder <redaelouahabi98@gmail.com>
2021-12-18 23:05:01 +0100

    adding all to github

--- Exo2f.cpp
+++ Exo2f.cpp
@@ -1,86 +1,75 @@
 //include section
-#include <iostream>
-//using section
-using namespace std;
-//main section
+	#include <iostream>
+	#include <cstdlib>
+		
+	//using section	
+	using std::cin;
+	using std::cout;
+	using std::endl;
+	
+	//main section
 int main()
 {
- // 10 rows 15 colums
+    // 10 rows 15 colums
     int rows = 10, columns = 15;
-
-//     int x[rows][columns] = {{0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 13, 0, 0, 0},
-// {9, 9, 9, 9, 9, 0, 0, 0, 0, 0, 13, 0, 13, 0, 0},
-// {0, 9, 0, 9, 0, 13, 13, 13, 13, 13, 13, 13, 13, 13, 0},
-// {9, 0, 9, 0, 9, 0, 13, 0, 0, 0, 0, 0, 13, 0, 1},
-// {0, 9, 0,  9,  0,   0, 17, 13, 0, 0, 0, 13, 0, 0, 1},
-// {9, 9, 9,  9,  9,  17, 0, 17, 13, 0, 13, 0, 0, 0, 2},
-// {0, 0, 0,  0,  17, 3, 0, 0, 17, 13, 0, 0, 5, 0, 1},
-// {2, 0, 0,  17, 0,  0, 0, 0, 0, 17, 0, 3, 1, 0, 1},
-// {0, 0, 17, 17, 17, 17, 17, 17, 17, 17, 17, 0, 0, 0, 1},
-// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}};
-    int x[rows][columns] = {};
-    int invertedX[rows][columns] ={};
-    // Scan user input array elements
+    int matrix[rows][columns] = {};
+    int invertedMatrix[rows][columns] ={};
+    // Scan user input for 2d array [Matrix]
     for(int i=0;i<rows;i++)
     {
         for(int j=0;j<columns;j++)
         {
-            //  dynamic approach
-            cout << "x[" << i << "][" << j << "] : ";
-            cin >> x[i][j];
+            cout << i << " * " << j << " = ";
+            cin >> matrix[i][j];
         }
         cout<<endl;
     }
-
-     // Fill In invertesX and print X
+     // Fill In invertedMatrix
+     // we invert rows of normal matrix Upside down
     for(int i=0;i<rows;i++)
     {
         for(int j=0;j<columns;j++)
         {
-            invertedX[rows-i-1][j]=x[i][j];
+            invertedMatrix[rows-i-1][j]=matrix[i][j];
         }
     }
-    // A : count triangles that has max dept
-    // B : mAx Dept
+    // A : count triangles that has max triangle_size
+    // B : Maximum triangle size [triangle_size]
     int A=0,B=0;
-    // serach traingle on x
-     for(int i=0;i<rows;i++)
+    // serach traingle on normal matrix
+    for(int i=0;i<rows;i++)
     {
         for(int j=0;j<columns;j++)
         {
-            // cout<<x[i][j]<<"\t";
-            // cout << "x[" << i << "][" << j << "]: "<<x[i][j] <<endl;
-            int dept=1;
-            while(dept<rows){
+            int triangle_size=1;//initialise the triangle size
+            while(triangle_size<rows){
                 // we are not at extremities
-                if ((j - dept >= 0 && j + dept <= columns) && i + dept <= rows && x[i][j]!=0){
+                if ((j - triangle_size >= 0 && j + triangle_size <= columns) && i + triangle_size <= rows && matrix[i][j]!=0){
                     // see if element in diag has the same value
-                    if (x[i][j]== x[i+dept][j-dept] ||  x[i][j] == x[i + dept][j + dept])
+                    if (matrix[i][j]== matrix[i+triangle_size][j-triangle_size] ||  matrix[i][j] == matrix[i + triangle_size][j + triangle_size])
                         {
-                            // cout << "[" << i << " : " << j << " ] :" << x[i][j] << " = G: " << x[i + dept][j - dept] << " && D:  " << x[i + dept][j + dept] << " Dept = " << dept << endl;
-                            if (dept >= 2){
-                                bool sameBase = false;
-                                // the rol how node od triangle disperse [dept=0,disp=0], [1,3],[2,5] ..
-                                // so we calsulate the distance = j+dept - (j-dept) +1
-                                for (int p = 0; p < (2 *dept)+ 1; p++)
+                            if (triangle_size >= 2){
+                                bool sameBase = false;//we suppose at first that the diagonal base has not the same values at row
+                                // the role how node of triangle disperse [triangle_size=0,disp=0], [1,3],[2,5] ..
+                                // so we calsulate the distance = j+triangle_size - (j-triangle_size) +1
+                                for (int p = 0; p < (2 *triangle_size)+ 1; p++)
                                 {
-                                    // cout << x[i+dept][j-dept] << endl;
-                                    if(x[i][j] ==  x[i+dept][j-dept]){
-                                        sameBase=true;
+                                    if(matrix[i][j] ==  matrix[i+triangle_size][j-triangle_size]){
+                                        sameBase=true; //we found same base
                                     }
                                     else{
                                         sameBase=false;
                                     }
                                 }
-                                if (sameBase)
+                                if (sameBase)//we have a real triangle
                                 {
-                                    cout << i << " " <<  j <<": " << dept+1 << " standing. Value: " << x[i][j] <<endl;
-                                    if(B<dept+1){
-                                        B=dept+1;
+                                    cout << i << " " <<  j <<": " << triangle_size+1 << " standing. Value: " << matrix[i][j] <<endl;
+                                    if(B<triangle_size+1){//this is the first triangles that has max triangle_size
+                                        B=triangle_size+1;//set the max size
                                         A=1;
                                     }
-                                    else if(B==dept+1){
-                                        ++A;
+                                    else if(B==triangle_size+1){
+                                        ++A;//increment counter of triangles that has max triangle_size
                                     }
                                     break;
                                 }
@@ -89,29 +78,27 @@ int main()
                         else{ break;}
                 }
                 else{ break;}
-                ++dept;
+                ++triangle_size;
             }
         }
     }
-     // serach traingle on invertedX [same logic as normal array 'x']
+     // serach traingle on invertedMatrix [same logic as normal array 'matrix']
     for(int i=0;i<rows;i++)
     {
         for(int j=0;j<columns;j++)
         {
-            int dept=1;
-            while(dept<rows){
+            int triangle_size=1;
+            while(triangle_size<rows){
                 // we are not at extremities
-                if ((j - dept >= 0 && j + dept <= columns) && i + dept <= rows && invertedX[i][j] !=0){
+                if ((j - triangle_size >= 0 && j + triangle_size <= columns) && i + triangle_size <= rows && invertedMatrix[i][j] !=0){
                     // see if element in diag has the same value
-                    if (invertedX[i][j]== invertedX[i+dept][j-dept] ||  invertedX[i][j] == invertedX[i + dept][j + dept])
+                    if (invertedMatrix[i][j]== invertedMatrix[i+triangle_size][j-triangle_size] ||  invertedMatrix[i][j] == invertedMatrix[i + triangle_size][j + triangle_size])
                         {
-                            if (dept >= 2){
+                            if (triangle_size >= 2){
                                 bool sameBase = false;
-                                // the rol how node od triangle disperse [dept=0,disp=0], [1,3],[2,5] ..
-                                // so we calsulate the distance = j+dept - (j-dept) +1
-                                for (int p = 0; p < (2 *dept)+ 1; p++)
+                                for (int p = 0; p < (2 *triangle_size)+ 1; p++)
                                 {
-                                    if(invertedX[i][j] ==  invertedX[i+dept][j-dept]){
+                                    if(invertedMatrix[i][j] ==  invertedMatrix[i+triangle_size][j-triangle_size]){
                                         sameBase=true;
                                     }
                                     else{
@@ -120,12 +107,12 @@ int main()
                                 }
                                 if (sameBase)
                                 {
-                                    cout << rows-i-1 << " " <<  j <<": " << dept+1 << " inverted. Value: " << invertedX[i][j] <<endl;
-                                    if(B<dept+1){
-                                        B=dept+1;
+                                    cout << rows-i-1 << " " <<  j <<": " << triangle_size+1 << " inverted. Value: " << invertedMatrix[i][j] <<endl;
+                                    if(B<triangle_size+1){
+                                        B=triangle_size+1;
                                         A=1;
                                     }
-                                    else if(B==dept+1){
+                                    else if(B==triangle_size+1){
                                         ++A;
                                     }
                                     break;
@@ -135,7 +122,7 @@ int main()
                         else{ break;}
                 }
                 else{ break;}
-                ++dept;
+                ++triangle_size;
             }
         }
     }
--- Exo3f.cpp
+++ Exo3f.cpp
@@ -1,50 +1,56 @@
 //include section
-#include <iostream>
-//using section
-using namespace std;
+	#include <iostream>
+	#include <cstdlib>
+		
+	//using section	
+	using std::cin;
+	using std::cout;
+	using std::endl;
 
-int find_patterns(int two_D_array[5][17], int one_D_array[5] , int& MaxSumOfRowCorrectPattern ){
-    int countPatterns =0;
-    int index1D =0;
+// the find_pattern function
+int find_patterns(int two_D_array[5][17], int set_of_rules[5] , int& MaxSumOfRowCorrectPattern ){
+    int countPatterns =0;// the counter that we will return at the end
+    int index1D =0; //the index used for looping set_of_rules array
  for(int i=0;i<5;i++){
      for(int j=0;j<17;j++)
         {
-            int RowSum =0;
-            index1D=0;
-            // fist elemnet correct
-            if((two_D_array[i][j] %2 ==0 && one_D_array[index1D]==0) ||
-             (two_D_array[i][j] >=0  && one_D_array[index1D]==1) ||
-             (two_D_array[i][j] %3 ==0 && one_D_array[index1D]==2) ){
-                //  we move index of One_D _Array to nex critaria;
-                 ++index1D;
-                RowSum+= two_D_array[i][j];
-                //  second and third corrects
-                    if((two_D_array[i][j+1] %2 ==0 && two_D_array[i][j+2] %2 ==0 && one_D_array[index1D]==0) ||
-             (two_D_array[i][j+1] >=0 && two_D_array[i][j+2] >=0 && one_D_array[index1D]==1) ||
-             (two_D_array[i][j+1] %3 ==0 && two_D_array[i][j+2] %3 ==0 && one_D_array[index1D]==2)){
+            int RowSum =0;// initiate Sum of current Row  [i]
+            index1D=0; //set to cursor on first element of set_of_rules
+            // if fist element is correct
+            if((two_D_array[i][j] %2 ==0 && set_of_rules[index1D]==0) ||
+             (two_D_array[i][j] >=0  && set_of_rules[index1D]==1) ||
+             (two_D_array[i][j] %3 ==0 && set_of_rules[index1D]==2) ){
+                
+                 ++index1D;//  we move index of One_D _Array to next critaria
+                RowSum+= two_D_array[i][j];//add this element tested to Sum of Row
+                // if second and third are corrects
+                    if((two_D_array[i][j+1] %2 ==0 && two_D_array[i][j+2] %2 ==0 && set_of_rules[index1D]==0) ||
+             (two_D_array[i][j+1] >=0 && two_D_array[i][j+2] >=0 && set_of_rules[index1D]==1) ||
+             (two_D_array[i][j+1] %3 ==0 && two_D_array[i][j+2] %3 ==0 && set_of_rules[index1D]==2)){
                   ++index1D;
                 RowSum+= two_D_array[i][j+1] + two_D_array[i][j+2];
-            // 4th 5th & 6th are corrects
-            if((two_D_array[i][j+3] %2 ==0 && two_D_array[i][j+4] %2 ==0 && two_D_array[i][j+5] %2 ==0 && one_D_array[index1D]==0) ||
-             (two_D_array[i][j+3] >=0 && two_D_array[i][j+4] >=0 && two_D_array[i][j+5] >=0 && one_D_array[index1D]==1) ||
-             (two_D_array[i][j+3] %3 ==0 && two_D_array[i][j+4] %3 ==0 && two_D_array[i][j+5] %3 ==0 && one_D_array[index1D]==2)){
+            // if 4th 5th & 6th are corrects
+            if((two_D_array[i][j+3] %2 ==0 && two_D_array[i][j+4] %2 ==0 && two_D_array[i][j+5] %2 ==0 && set_of_rules[index1D]==0) ||
+             (two_D_array[i][j+3] >=0 && two_D_array[i][j+4] >=0 && two_D_array[i][j+5] >=0 && set_of_rules[index1D]==1) ||
+             (two_D_array[i][j+3] %3 ==0 && two_D_array[i][j+4] %3 ==0 && two_D_array[i][j+5] %3 ==0 && set_of_rules[index1D]==2)){
                   ++index1D;
                 RowSum+= two_D_array[i][j+3] + two_D_array[i][j+4] + two_D_array[i][j+5];
-            // 7th 8th 9th 10th are corrects
-              if((two_D_array[i][j+6] %2 ==0 && two_D_array[i][j+7] %2 ==0 && two_D_array[i][j+8] %2 ==0 && two_D_array[i][j+9] %2 ==0 && one_D_array[index1D]==0) ||
-             (two_D_array[i][j+6] >=0 && two_D_array[i][j+7] >=0 && two_D_array[i][j+8] >=0 && two_D_array[i][j+9] >=0 && one_D_array[index1D]==1) ||
-             (two_D_array[i][j+6] %3 ==0 && two_D_array[i][j+7] %3 ==0 && two_D_array[i][j+8] %3 ==0 && two_D_array[i][j+9] %3 ==0 && one_D_array[index1D]==2)){
+            // if 7th 8th 9th 10th are corrects
+              if((two_D_array[i][j+6] %2 ==0 && two_D_array[i][j+7] %2 ==0 && two_D_array[i][j+8] %2 ==0 && two_D_array[i][j+9] %2 ==0 && set_of_rules[index1D]==0) ||
+             (two_D_array[i][j+6] >=0 && two_D_array[i][j+7] >=0 && two_D_array[i][j+8] >=0 && two_D_array[i][j+9] >=0 && set_of_rules[index1D]==1) ||
+             (two_D_array[i][j+6] %3 ==0 && two_D_array[i][j+7] %3 ==0 && two_D_array[i][j+8] %3 ==0 && two_D_array[i][j+9] %3 ==0 && set_of_rules[index1D]==2)){
              ++index1D;
                 RowSum+= two_D_array[i][j+6] + two_D_array[i][j+7] + two_D_array[i][j+8] + two_D_array[i][j+9];
-            // 11th 12th 13th 14th 15th are corrects
-            if((two_D_array[i][j+10] %2 ==0 && two_D_array[i][j+11] %2 ==0 && two_D_array[i][j+12] %2 ==0 && two_D_array[i][j+13] %2 ==0  && two_D_array[i][j+14] %2 ==0 && one_D_array[index1D]==0) ||
-             (two_D_array[i][j+10] >=0 && two_D_array[i][j+11] >=0 && two_D_array[i][j+12] >=0 && two_D_array[i][j+13] >=0 && two_D_array[i][j+14] >=0 && one_D_array[index1D]==1) ||
-             (two_D_array[i][j+10] %3 ==0 && two_D_array[i][j+11] %3 ==0 && two_D_array[i][j+12] %3 ==0 && two_D_array[i][j+13] %3 ==0 && two_D_array[i][j+14] %3 ==0 && one_D_array[index1D]==2)){
+            // if 11th 12th 13th 14th 15th are corrects
+            if((two_D_array[i][j+10] %2 ==0 && two_D_array[i][j+11] %2 ==0 && two_D_array[i][j+12] %2 ==0 && two_D_array[i][j+13] %2 ==0  && two_D_array[i][j+14] %2 ==0 && set_of_rules[index1D]==0) ||
+             (two_D_array[i][j+10] >=0 && two_D_array[i][j+11] >=0 && two_D_array[i][j+12] >=0 && two_D_array[i][j+13] >=0 && two_D_array[i][j+14] >=0 && set_of_rules[index1D]==1) ||
+             (two_D_array[i][j+10] %3 ==0 && two_D_array[i][j+11] %3 ==0 && two_D_array[i][j+12] %3 ==0 && two_D_array[i][j+13] %3 ==0 && two_D_array[i][j+14] %3 ==0 && set_of_rules[index1D]==2)){
                 //  Here the Pattern is Found
+                // we add last elements to Sum
                 RowSum+= two_D_array[i][j+10] + two_D_array[i][j+11] + two_D_array[i][j+12] + two_D_array[i][j+13] +  two_D_array[i][j+14];
-                ++countPatterns;
+                ++countPatterns; //we increment our counter of pattern
                 if(RowSum > MaxSumOfRowCorrectPattern){
-                    MaxSumOfRowCorrectPattern =RowSum;
+                    MaxSumOfRowCorrectPattern =RowSum; //we make sure that we will have the max Sum of rows
                 }
              }
              }
@@ -60,30 +66,18 @@ int main()
 {
  //  5 rows 17 columns
     int rows = 5, columns = 17;
-    // dynamic approach
-    int x[rows][17] = {};
-    
-    //static approach
-    // int x[rows][17]={
-    //     {3,-1,3,3,3,2,3,3,3,3,3,3,3,3,3,3,3},
-    //     {2,1,3,2,3,4,0,0,7,7,6,77,6,6,66,6,0},
-    //     {1,-2,3,2,4,-8,252,6,7,8,9,-6,6,9,-6,9,-4},
-    //     {1,3,5,-5,5,3,17,12,12,3,-2,5,7,9,12,66,7},
-    //     {12,3,24,4,8,-2,12,3,3,3,3,3,3,3,3,3,3},
-    // };
+    // define the two D array
+    int matrix[rows][17] = {};
     // set N to be 5
     int N=5;
     int set_of_rules[N] = {0,1,0,1,2};
-    enum patterns_t
-    {   EVEN_PT, POSITIVE_PT, DIVIDE_BY_3_MPT};
-
-    // Scan user input array elements
+    // Scan user input for 2d array [Matrix]
     for(int i=0;i<rows;i++)
     {
         for(int j=0;j<columns;j++)
         {
             cout << i << " * " << j << " = ";
-            cin >> x[i][j];
+            cin >> matrix[i][j];
         }
         cout<<endl;
     }
@@ -97,7 +91,8 @@ int main()
     // Define the reference parrametter
     int MaxSumOfRowCorrectPattern{ -1 };
     // call find_patterns function
-    int countPatterns = find_patterns(x,set_of_rules,MaxSumOfRowCorrectPattern);
-    cout << countPatterns << MaxSumOfRowCorrectPattern<<endl;
+    int countPatterns = find_patterns(matrix,set_of_rules,MaxSumOfRowCorrectPattern);\
+    // print the result
+    cout << countPatterns <<" "<< MaxSumOfRowCorrectPattern<<endl;
 	return EXIT_SUCCESS;
 }
